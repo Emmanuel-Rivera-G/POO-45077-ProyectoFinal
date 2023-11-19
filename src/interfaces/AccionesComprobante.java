6ee12d72b5ee6eb05c52c0fd8e2ae2ca26c8bb5d
Emmanuel-Rivera-G <114590626+Emmanuel-Rivera-G@users.noreply.github.com>
2023-11-19 12:12:14 -0500

    Delete src directory

--- src/interfaces/AccionesComprobante.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface AccionesComprobante {
-	
-	public double calcularDescuento();
-	public double calcularTotal();
-	public boolean anularComprobante();	
-	
-}
--- src/interfaces/Pagos.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface Pagos {
-	public double CalcularPagos();
-}
